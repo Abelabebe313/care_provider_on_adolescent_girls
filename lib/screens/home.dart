d13cc23339ba5c408abe0297a7efb85c56e77a31
Abelabebe313 <abelabebe313@gmail.com>
2023-09-11 22:44:41 +0300

    added some content on home

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
@@ -8,14 +9,59 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  final List<String> title_list = [
+    'INTRODUCTION',
+    'ADOLESCENT SEXUAL AND REPRODUCTIVE HEALTH SERVICE',
+    'CONTRACEPTIVE COUNSELLING AND SERVICES',
+    'COMPREHENSIVE ABORTION CARE',
+    'Sexually-Transmitted Infections Preventions, Control and Treatment',
+    'HIV PREVENTION, CONTROL AND TREATMENT',
+    'Antenatal, Intrapartum and Postnatal Care Services',
+    'GENDER–BASED VIOLENCE SERVICES',
+    'ADOLESCENT NUTRITION SERVICES',
+    'MONITORING',
+    'SUMMARY',
+  ];
+
+  late PageController _pageController;
+  late Timer _timer;
+  int _currentPage = 0;
+  int current = 0;
+  @override
+  void initState() {
+    super.initState();
+    _pageController = PageController();
+    _startAutoScroll();
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    _timer.cancel();
+    super.dispose();
+  }
+
+  void _startAutoScroll() {
+    _timer = Timer.periodic(Duration(seconds: 5), (timer) {
+      if (_currentPage < 4) {
+        _currentPage++;
+      } else {
+        _currentPage = 0;
+      }
+      _pageController.animateToPage(
+        _currentPage,
+        duration: Duration(seconds: 1),
+        curve: Curves.easeInOut,
+      );
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        backgroundColor: const Color(0xffF8FAFF),
         appBar: AppBar(
-          // leading: IconButton(
-          //   onPressed: () {},
-          //   icon: Icon(Icons.menu),
-          // ),
+          backgroundColor: const Color(0xffF8FAFF),
           title: Center(
             child: Container(
               padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
@@ -24,48 +70,163 @@ class _HomeState extends State<Home> {
           ),
         ),
         body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
+            const SizedBox(
+              height: 10,
+            ),
             // Search Bar
-            Expanded(
-              child: TextField(
-                decoration: InputDecoration(
-                  hintText: "Search an article...",
-                  hintStyle: TextStyle(
-                    fontFamily: 'Poppins-ExtraLight',
-                    color: Colors.grey.shade400,
-                    fontSize: 15,
-                  ),
-                  contentPadding: const EdgeInsets.only(
-                      left: 10, top: 12, bottom: 12), // Add padding here
-                  border: OutlineInputBorder(
-                    
-                    borderSide: BorderSide(
-                      color: Colors.grey.shade300,
-                      width: 1,
+            Center(
+              child: Container(
+                height: 50,
+                width: MediaQuery.of(context).size.width * 0.95,
+                decoration: BoxDecoration(
+                  color: Colors.grey.shade100,
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: TextField(
+                  decoration: InputDecoration(
+                    hintText: "Search an article...",
+                    hintStyle: TextStyle(
+                      fontFamily: 'Poppins-ExtraLight',
+                      color: Colors.grey.shade400,
+                      fontSize: 15,
                     ),
-                    borderRadius: const BorderRadius.all(
-                      Radius.circular(10),
-                    ),
-                  ),
-                  suffixIcon: Container(
-                    width: 50,
-                    height: 50,
-                    decoration: BoxDecoration(
-                      color: const Color(0xFF0E5120),
-                      borderRadius: BorderRadius.circular(12),
-                    ),
-                    child: IconButton(
-                      icon: const Icon(
-                        Icons.search,
-                        size: 32,
+                    contentPadding: const EdgeInsets.only(
+                        left: 10, top: 12, bottom: 12), // Add padding here
+                    border: InputBorder.none,
+                    suffixIcon: Container(
+                      width: 50,
+                      height: 50,
+                      decoration: BoxDecoration(
+                        color: const Color(0xFF0E5120),
+                        borderRadius: BorderRadius.circular(12),
                       ),
-                      color: Colors.white,
-                      onPressed: () {},
+                      child: IconButton(
+                        icon: const Icon(
+                          Icons.search,
+                          size: 32,
+                        ),
+                        color: Colors.white,
+                        onPressed: () {},
+                      ),
                     ),
                   ),
                 ),
               ),
             ),
+
+            const SizedBox(
+              height: 10,
+            ),
+
+            // Slider poster
+            Container(
+              height: 150,
+              child: PageView(
+                controller: _pageController,
+                physics: NeverScrollableScrollPhysics(),
+                // scrollDirection: Axis.horizontal,
+                children: [
+                  SizedBox(
+                    width: (MediaQuery.of(context).size.width),
+                    child: ClipRRect(
+                      child: Image.asset(
+                        'assets/images/slider 1.png',
+                        fit: BoxFit.cover,
+                        width: double.infinity,
+                        height: double.infinity,
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: (MediaQuery.of(context).size.width),
+                    child: ClipRRect(
+                      child: Image.asset(
+                        'assets/images/slider 2.png.jpg',
+                        fit: BoxFit.cover,
+                        width: double.infinity,
+                        height: double.infinity,
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: (MediaQuery.of(context).size.width),
+                    child: ClipRRect(
+                      child: Image.asset(
+                        'assets/images/slider 3.jpg',
+                        fit: BoxFit.cover,
+                        width: double.infinity,
+                        height: double.infinity,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+
+            const SizedBox(
+              height: 10,
+            ),
+            const Padding(
+              padding: EdgeInsets.symmetric(
+                horizontal: 10,
+                vertical: 5,
+              ),
+              child: Text(
+                'Catagories',
+                style: TextStyle(
+                  fontFamily: 'Urbanist-Regular',
+                  color: const Color(0xFF0E5120),
+                  fontSize: 20,
+                ),
+              ),
+            ),
+
+            Padding(
+              padding: const EdgeInsets.only(left: 10),
+              child: Container(
+                color: const Color(0xffF8FAFF),
+                width: MediaQuery.of(context).size.width * 0.93,
+                height: MediaQuery.of(context).size.height * 0.58,
+                child: ListView.builder(
+                  itemCount: title_list.length,
+                  itemBuilder: (context, index) {
+                    return Container(
+                      margin: EdgeInsets.only(bottom: 10),
+                      width: MediaQuery.of(context).size.width * 0.9,
+                      height: MediaQuery.of(context).size.height * 0.18,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      child: Row(
+                        children: [
+                          const SizedBox(
+                            width: 10,
+                          ),
+                          Container(
+                            width: 120,
+                            child: Image.asset('assets/images/counselling.png'),
+                          ),
+                          Container(
+                            width: 250,
+                            child: Text(
+                              title_list[index],
+                              style: const TextStyle(
+                                fontFamily: 'Urbanist-Regular',
+                                // color: Color(0xFF0E5120),
+                                fontSize: 20,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                ),
+              ),
+            )
           ],
         ));
   }

fd13070f6180b55975542b031f8cca4c057089e1
Gemechis <gemeelijah@gmail.com>
2023-09-11 19:25:21 +0300

    PDF Viewer added

---
+++ lib/screens/pdf_viewer/view.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_pdfviewer/pdfviewer.dart';
+import 'package:syncfusion_flutter_pdf/pdf.dart';
+
+class PDFView extends StatefulWidget {
+  final String title;
+  final String fileName;
+  const PDFView({super.key, required this.title, required this.fileName});
+
+  @override
+  _PDFView createState() => _PDFView();
+}
+
+class _PDFView extends State<PDFView> {
+  late PdfViewerController _pdfViewerController;
+  late PdfTextSearchResult _searchResult;
+
+  @override
+  void initState() {
+    _pdfViewerController = PdfViewerController();
+    _searchResult = PdfTextSearchResult();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(
+              Icons.search,
+              color: Color.fromARGB(255, 15, 15, 15),
+            ),
+            onPressed: () {
+              _searchResult = _pdfViewerController.searchText('',
+                  searchOption: TextSearchOption.caseSensitive);
+              if (kIsWeb) {
+                setState(() {});
+              } else {
+                _searchResult.addListener(() {
+                  if (_searchResult.hasResult) {
+                    setState(() {});
+                  }
+                });
+              }
+            },
+          ),
+          Visibility(
+            visible: _searchResult.hasResult,
+            child: IconButton(
+              icon: const Icon(
+                Icons.clear,
+                color: Color.fromARGB(255, 15, 15, 15),
+              ),
+              onPressed: () {
+                setState(() {
+                  _searchResult.clear();
+                });
+              },
+            ),
+          ),
+          Visibility(
+            visible: _searchResult.hasResult,
+            child: IconButton(
+              icon: const Icon(
+                Icons.keyboard_arrow_up,
+                color: Color.fromARGB(255, 15, 15, 15),
+              ),
+              onPressed: () {
+                _searchResult.previousInstance();
+              },
+            ),
+          ),
+          Visibility(
+            visible: _searchResult.hasResult,
+            child: IconButton(
+              icon: const Icon(
+                Icons.keyboard_arrow_down,
+                color: Color.fromARGB(255, 15, 15, 15),
+              ),
+              onPressed: () {
+                _searchResult.nextInstance();
+              },
+            ),
+          ),
+        ],
+      ),
+      body: SfPdfViewer.asset(
+        'assets/files/${widget.fileName}',
+        controller: _pdfViewerController,
+        currentSearchTextHighlightColor: Colors.yellow.withOpacity(0.6),
+        otherSearchTextHighlightColor: Colors.yellow.withOpacity(0.3),
+      ),
+    );
+  }
+}
